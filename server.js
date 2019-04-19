a2c13ecc00d4896ae28cc7c261784ecba7d37bce
atiftariq786 <atiftariq786@hotmail.com>
2019-04-18 21:08:04 -0700

    server.js pakages setup and handlebars setup configure

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var mongoose = require("mongoose");
-var exhbs = require("express-handlebars");
-
+var exphbs = require("express-handlebars");
+var bodyParser = require('body-parser')
 
 
 // Our scraping tools
@@ -10,16 +10,25 @@ var exhbs = require("express-handlebars");
 var axios = require("axios");
 var cheerio = require("cheerio");
 
+// Port Configuration 
+var PORT       = process.env.PORT || 3000;
+// Mongodb connection
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoscraper";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+  useMongoClient: true
+});
+
 // Require all models
 var db = require("./models");
-
-var PORT = 3000;
 
 // Initialize Express
 var app = express();
 
-// Configure middleware
-
+// Handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
@@ -27,8 +36,10 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
-// Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+
+
+
+
 
 
 // Start the server
